b73378af6a711f63084721cf715031d98f3f31c8
Your Name <JulienneJulien15@gmail.com>
2023-02-23 18:07:00 -0500

    added user login and logout to userRoute

--- Develop/controllers/api/userRoutes.js
+++ Develop/controllers/api/userRoutes.js
@@ -69,3 +69,49 @@ router.post("/", (req, res) => {
     });
   });
   
+  //  login user
+router.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({
+        where: {
+          user_name: req.body.user_name,
+        },
+      });
+  
+      if (!user) {
+        res.status(400).json({ message: 'No user account found!' });
+        return;
+      }
+  
+      const validPassword = user.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect Password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.userId = user.id;
+        req.session.username = user.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(400).json({ message: 'No user account found!' });
+    }
+  });
+
+  //  logout user
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      console.log(req.session.loggedIn)
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
+module.exports = router;
