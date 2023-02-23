a52645e69617ae612c96aa3fb97e16d2434b8d17
Your Name <JulienneJulien15@gmail.com>
2023-02-23 18:00:36 -0500

    added get and post to userroutes

--- Develop/utils/auth.js
+++ Develop/utils/auth.js
@@ -1,4 +1,5 @@
 const withAuth = (req, res, next) => {
+    // TO send request back to the login route if user is not logged in.
     if (!req.session.userId) {
       res.redirect("/login");
     } else {
