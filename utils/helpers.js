59a6e1b09ae438aa8a2a32bff471c2b8cda8d829
Your Name <JulienneJulien15@gmail.com>
2023-02-24 20:38:49 -0500

    dashboard user posts successful hovewever still receiving error for comments

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,13 @@
 module.exports = {
     format_date: date => {
       return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    }
+    },
+    format_plural: (word, amount) => {
+      if (amount !== 1) {
+          return `${word}s`
+      }
+
+      return word;
+  }
   };
   
